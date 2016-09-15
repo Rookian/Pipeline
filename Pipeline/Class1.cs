70d19a002ff5296e52a026245446d7974663e3a2
Rookian <Rookian@gmx.de>
2016-09-15 23:01:07 +0200

    1st commit

--- Pipeline/Class1.cs
+++ Pipeline/Class1.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pipeline
+﻿namespace Pipeline
 {
+    public interface IFunction<in TIn, out TOut>
+    {
+        TOut Process(TIn @in);
+    }
+
+    public class Nothing
+    {
+
+    }
+
+    public class Data
+    {
+        public string Name { get; set; }
+    }
+
+    public class DataViewModel
+    {
+        public string Name { get; set; }
+    }
+
+    public class ReadData : IFunction<Nothing, Data>
+    {
+        public Data Process(Nothing @in)
+        {
+            return new Data { Name = "Test" };
+        }
+    }
+
+    public class MapData : IFunction<Data, DataViewModel>
+    {
+        public DataViewModel Process(Data @in)
+        {
+            return new DataViewModel { Name = @in.Name };
+        }
+    }
+
     public class Class1
     {
+        public void Do()
+        {
+            var readData = new ReadData();
+            var mapData = new MapData();
+
+            readData.Process(new Nothing());
+        }
     }
 }
